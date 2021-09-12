5bfc92209e9c5f18d6a0b24846d1cf0f65ac1e52
Marthely <jeadestetsiba@gmail.com>
2021-09-12 22:49:29 +0200

    delete all console log

--- Vue/fetchVerse.js
+++ Vue/fetchVerse.js
@@ -4,7 +4,7 @@ function setQuestion(code){
     verseOfCode.index = bookCode(verseOfCode.book)
     verseOfCode.chapiter = parseInt(verseOfCode.chapiter)
     verseOfCode.verse = parseInt(verseOfCode.verse)
-    console.log(verseOfCode);
+    // console.log(verseOfCode);
     clearQuestion('<img src="./vendor/animate/Ellipsis.gif" height="150em"/>')
     let verse_txt = fr_apee[verseOfCode.index].chapters[(verseOfCode.chapiter-1)][(verseOfCode.verse-1)]
     // console.log(verse_txt); return
@@ -14,5 +14,5 @@ function setQuestion(code){
     document.getElementById('codeQuestion').setAttribute('disabled','')
     document.getElementById('validationBTN').removeAttribute('hidden','')
     document.getElementById('btn-msg').setAttribute('hidden','')
-    console.log(true_answered)
+    // console.log(true_answered)
 }
